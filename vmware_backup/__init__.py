8bfd817ac5ac81b42e8a36f3cf50c5e6da535534
Barmaley13 <kbelyayev@gmail.com>
2016-05-09 15:10:29 -0600

    Updating setup 1

--- vmware_backup/__init__.py
+++ vmware_backup/__init__.py
@@ -25,6 +25,6 @@ from .cron import enable_backup, disable_backup
 
 ### CONSTANTS ###
 ## Meta Data##
-__version__ = '1.02.11'
+__version__ = '1.02.12'
 __author__ = 'Kirill V. Belyayev'
 __license__ = 'GPL'
